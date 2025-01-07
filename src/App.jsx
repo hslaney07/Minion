5968ddb2383715392e5124495ae023f36d11e3a9
Haley <hgslaney@wpi.edu>
2025-01-07 11:01:52 -0500

    began implementing pop ups

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
   const [userData, setUserData] = useState(null);
 
   const CLIENT_ID = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
-  const REDIRECT_URI = 'http://127.0.0.1:5173/Spotify/';
+  const REDIRECT_URI = 'http://localhost:5173/Spotify/';
   const SCOPES = 'user-read-private user-read-email user-top-read playlist-modify-public playlist-modify-private';
   const AUTH_URL = useMemo(() => {
     return `https://accounts.spotify.com/authorize?response_type=token&client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&scope=${encodeURIComponent(SCOPES)}`;
@@ -25,7 +25,7 @@ const App = () => {
       if (accessToken) {
         localStorage.setItem('spotifyToken', accessToken);
         // Redirect to the main app without the token in the URL
-        window.location.replace('http://127.0.0.1:5173/Spotify/') 
+        window.location.replace('http://localhost:5173/Spotify/') 
       }
     }
   }, []);
